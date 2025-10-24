fb5928e3b743c6ab8ce9f4ae8c25392d64d0c838
Lopez Tellez Katherin Alexandra <kalopezt@ollamani.com>
2025-10-24 11:40:19 -0600

    Prueba

--- Domain.Test/obj/Debug/net8.0/Domain.Test.AssemblyInfo.cs
+++ Domain.Test/obj/Debug/net8.0/Domain.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3baf8f67e296f283ea828c4c87e8653575a8edd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152f1a95586554e44c4303c7ac0737aca18e73a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
